ffe3b3866ac6a7c3d68e292671605d1ea8653861
Bdeveloper <bhushansonyx@gmail.com>
2024-10-26 11:53:25 +0530

    check for empty string

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function countStringValue (str) {
+    if(str.trim() === "" ) {
+        return 0;
+    } else {
+        console.log(`valid string: ${str}`)
+    }
+
+
+}
+
+countStringValue("");
+countStringValue("1");
