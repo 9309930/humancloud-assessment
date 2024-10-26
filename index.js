22fb4f6d48f80808a0b0c84b51268d72c1fcd016
Bdeveloper <bhushansonyx@gmail.com>
2024-10-26 12:17:33 +0530

    clean string, format numbers

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 
-function countStringValue (str) {
-    if(str.trim() === "" ) {
-        return 0;
-    } else {
-        console.log(`valid string: ${str}`)
-    }
+function sumNumbersInString (str) {
+    
+        if (str.trim() === "") return 0; 
+      
+        const cleanedStr = str.replace(/[\/,;*\\]+/g, "");
+      
+        const numbers = cleanedStr.split(/[^-0-9]/).filter(Boolean);
+      
+        const sum = [];
+        const negatives = []; 
+      
+        numbers.forEach(numStr => {
+          const num = parseInt(numStr, 10);
+          console.log(num);
+      
+          if (num < 0) {
+            negatives.push(num); 
+          } else if (num <= 1000) {
+            sum.push(num)
+          }
+        });
+      
+      
+        return sum;
+      }
+      
+      try {
+        (sumNumbersInString("1,5"));
+        (sumNumbersInString("")); 
+        (sumNumbersInString("10//2,3\\,4*1001"));
+        (sumNumbersInString("1//n;,-2,3\\5"));
+    
+      } catch (error) {
+        console.error(error.message);
+      }
+      
 
 
-}
 
-countStringValue("");
-countStringValue("1");
+
+
+
+
